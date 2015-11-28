737377083107bd365c67c65f955cfc5feb4edec4
Andrew Jones <ajon542@gmail.com>
2015-11-27 22:14:52 -0800

    Comments

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,14 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            // Opening a connection for each message is just not going to work.
+            // This is ok for the client to get information from the server, but there
+            // is no way for the server to send a message to the client this way.
+            // We must leave the socket open when the client connects. However, I have
+            // encountered some issues when closing the connection under these circumstances.
+            // For example, if we start a BeginReceive and then close the connection, it seems
+            // to get an exception because something in the receive callback comes through
+            // after the connection is closed.
             Client client = new Client();
             client.StartClient();
 
