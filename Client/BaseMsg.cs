82ab6807022b026f16cffaa7802349fbbcb29c82
Andrew Jones <ajon542@gmail.com>
2015-12-04 17:10:11 -0800

    Added BaseMsg for client

---
+++ Client/BaseMsg.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Client
+{
+    /// <summary>
+    /// Base class for all messages.
+    /// </summary>
+    public class BaseMsg
+    {
+        /// <summary>
+        /// Gets or sets a string description for the type of message.
+        /// </summary>
+        /// <remarks>
+        /// This will be used in determining the type for (de)serialization.
+        /// </remarks>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contents of the message.
+        /// </summary>
+        public string Contents { get; set; }
+    }
+}
